804222821792752e7c0e505e92ed9142f1ed3c7e
Rayhana E <florayana97@hotmail.com>
2023-02-22 21:55:04 +0100

    css styles & created each file a component

---
+++ src/components/CountriesPast.tsx
@@ -0,0 +1,21 @@
+
+
+
+const CountriesPast = () => {
+    return (
+        <>
+         <ul className='box2'>
+ <p>Länder jag vill resa till:</p>
+ <li > Kanada</li>
+ <li> Kambodya</li>
+ <li> Kenya</li>
+ <li> Sydkorea</li>
+</ul>
+        </>
+       
+        
+    )
+}
+
+
+export default CountriesPast
--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,5 +1,8 @@
 import './display.css'
 import Pic from '../imgs/pic.jpg'
+import CountriesFuture from './CountriesFuture'
+import CountriesPast from './CountriesPast'
+
 
 
 
@@ -9,37 +12,31 @@ const Display = () => {
 
     return (
         <>
-        <p>min första uppgift</p>
-        
+        <h1>Mina Destinationer</h1>
+
         <div className="container">
 <div className ="search-container">
- <input type="text" placeholder="Lägg till..." />
+ <input type="text" placeholder="Lägg till..." className='placeh'/>
  <button>OK</button>
 <div className ="checkbox">
  <input type="checkbox" />
  Vill resa
  </div>
+
+
  <div className="checkbox">
  <input type="checkbox" />
  Har rest
  </div>
+
 </div>
- <ul>
- <p>Länder jag har rest till:</p>
- <li>- Norge</li>
- <li>- Kina</li>
- <li>- Vietnam</li>
- <li>- Spanien</li>
- </ul>
- <ul>
- <p>Länder jag vill resa till:</p>
- <li>- USA</li>
- <li>- Kambodya</li>
- <li>- Kenya</li>
- <li>- Danmark</li>
-</ul>
+
+<CountriesFuture></CountriesFuture>
+
+ <CountriesPast></CountriesPast>
+ 
+
 </div>
-h. Du får rött där d
 
         </>
     )
